9320782a285f4d1e5766031fcb123cf9fc6b2536
Miilenna <milena200.fv@gmail.com>
2024-11-23 13:00:41 +0100

    arxiu topic fet

--- mi-proyecto-iot/src/main/java/com/miempresa/clienteiot/TopicIoT.java
+++ mi-proyecto-iot/src/main/java/com/miempresa/clienteiot/TopicIoT.java
@@ -1,1 +1,16 @@
 package main.java.com.miempresa.clienteiot;
+
+import com.amazonaws.services.iot.client.AWSIotMessage;
+import com.amazonaws.services.iot.client.AWSIotQos;
+import com.amazonaws.services.iot.client.AWSIotTopic;
+
+public class TopicIoT extends AWSIotTopic{
+    public TopicIoT(String topic, AWSIotQos qos) {
+        super(topic, qos);
+    }
+
+    @Override
+    public void onMessage(AWSIotMessage message) {
+        System.out.println(System.currentTimeMillis()+": <<< " + message.getStringPayload());
+    }
+}
